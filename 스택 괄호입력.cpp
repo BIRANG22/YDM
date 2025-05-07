295d568a63cfcc8cd6d72950319abb0262339873
BIRANG22 <yundongmin9@gmail.com>
2025-05-07 23:44:58 +0900

    Create 스택 괄호입력.cpp
    코테 스택 괄호입력

---
+++ 스택 괄호입력.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+char stack[100];
+
+int main() {
+    int count = 0;
+    char input;
+    
+    while (true)
+    {
+        std::cout << "괄호를 입력해주세요: ";
+
+        std::cin >> input;
+        if (input == '(' || input == '{' || input == '[')
+        {
+            stack[count] = input;
+            count++;
+        }
+        else
+        {
+            if ((stack[count - 1] == '(' &&  input == ')') || (stack[count - 1] == '{' && input == '}') || (stack[count - 1] == '[' && input == ']'))
+            {
+                count--;
+                std::cout << "pop: " << stack[count ] << input << std::endl;
+                if (count == 0)break;
+            }
+            else
+            {
+                std::cout << "ERROR";
+            }
+        }
+    }
+    
+
+    return 0;
+}
