c2d3e59073ec84c1b9b1321825c77a75a1c5d7e9
BIRANG22 <yundongmin9@gmail.com>
2025-05-08 01:07:05 +0900

    Create 큐시스템.cpp
    코테 큐시스템 구현

---
+++ 큐시스템.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+int arr[100];
+
+int main()
+{
+    int front = 0;
+    int rear = 0;
+    int size = 100;
+    int input = 0;
+    while (true)
+    {
+        size = rear - front;
+
+        rear = (rear + 1) % 100;
+        front = (front + 1) % 100;
+
+        if (size == 99 || front - rear == 1)
+        {
+            std::cout << "fool";
+            break;
+        }
+        if (size == 0)
+        {
+            std::cout << "empty";
+        }
+
+        rear = (rear + 1) % 100;
+        front = (front + 1) % 100;
+
+        std::cout << "정수를 입력해주세요(종료:-1)(pop:-2): ";
+        std::cin >> input;
+        if (input == -1)break;
+        if (input == -2)
+        {
+            std::cout << "pop: " << arr[front] << std::endl;
+            front++;
+            std::cout << "front인덱스의 값: " << arr[front] << std::endl;
+            std::cout << "front 위치: " << front << std::endl;
+            std::cout << "rear 위치: " << rear << std::endl;
+            std::cout << "남은 배열 사이즈: " << size << std::endl;
+        }
+        else
+        {
+            arr[rear] = input;
+            rear++;
+            std::cout << "front인덱스의 값: " << arr[front] << std::endl;
+            std::cout << "front 위치: " << front << std::endl;
+            std::cout << "rear 위치: " << rear << std::endl;
+            std::cout << "남은 배열 사이즈: " << size << std::endl;
+        }
+    }
+        
+
+
+    return 0;
+}
+
